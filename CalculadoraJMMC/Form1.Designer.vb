fafc7e294ae0b0da6f6f001d0a37c7521fdc7e5b
Juan M <juanma887@gmail.com>
2020-10-05 23:29:42 +0200

    Base para calculadora III

---
+++ CalculadoraJMMC/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBoxResultado = New System.Windows.Forms.TextBox()
+        Me.btnNum0 = New System.Windows.Forms.Button()
+        Me.btnNum7 = New System.Windows.Forms.Button()
+        Me.btnNum8 = New System.Windows.Forms.Button()
+        Me.btnNum9 = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnNum4 = New System.Windows.Forms.Button()
+        Me.btnNum5 = New System.Windows.Forms.Button()
+        Me.btnNum6 = New System.Windows.Forms.Button()
+        Me.btnMult = New System.Windows.Forms.Button()
+        Me.btnNum1 = New System.Windows.Forms.Button()
+        Me.btnNum2 = New System.Windows.Forms.Button()
+        Me.btnNum3 = New System.Windows.Forms.Button()
+        Me.btnRest = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnMas = New System.Windows.Forms.Button()
+        Me.btnCE = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtBoxResultado
+        '
+        Me.txtBoxResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBoxResultado.Location = New System.Drawing.Point(3, 4)
+        Me.txtBoxResultado.MaxLength = 17
+        Me.txtBoxResultado.Name = "txtBoxResultado"
+        Me.txtBoxResultado.ReadOnly = True
+        Me.txtBoxResultado.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.txtBoxResultado.Size = New System.Drawing.Size(391, 53)
+        Me.txtBoxResultado.TabIndex = 1
+        Me.txtBoxResultado.Text = "0"
+        Me.txtBoxResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnNum0
+        '
+        Me.btnNum0.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.btnNum0, 3)
+        Me.btnNum0.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum0.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum0.Location = New System.Drawing.Point(3, 240)
+        Me.btnNum0.Name = "btnNum0"
+        Me.btnNum0.Size = New System.Drawing.Size(231, 75)
+        Me.btnNum0.TabIndex = 13
+        Me.btnNum0.Text = "0"
+        Me.btnNum0.UseVisualStyleBackColor = True
+        '
+        'btnNum7
+        '
+        Me.btnNum7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum7.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum7.Location = New System.Drawing.Point(3, 3)
+        Me.btnNum7.Name = "btnNum7"
+        Me.btnNum7.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum7.TabIndex = 0
+        Me.btnNum7.Text = "7"
+        Me.btnNum7.UseVisualStyleBackColor = True
+        '
+        'btnNum8
+        '
+        Me.btnNum8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum8.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum8.Location = New System.Drawing.Point(82, 3)
+        Me.btnNum8.Name = "btnNum8"
+        Me.btnNum8.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum8.TabIndex = 1
+        Me.btnNum8.Text = "8"
+        Me.btnNum8.UseVisualStyleBackColor = True
+        '
+        'btnNum9
+        '
+        Me.btnNum9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum9.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum9.Location = New System.Drawing.Point(161, 3)
+        Me.btnNum9.Name = "btnNum9"
+        Me.btnNum9.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum9.TabIndex = 2
+        Me.btnNum9.Text = "9"
+        Me.btnNum9.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDiv.Location = New System.Drawing.Point(240, 3)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(73, 73)
+        Me.btnDiv.TabIndex = 3
+        Me.btnDiv.Text = "/"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnNum4
+        '
+        Me.btnNum4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum4.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum4.Location = New System.Drawing.Point(3, 82)
+        Me.btnNum4.Name = "btnNum4"
+        Me.btnNum4.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum4.TabIndex = 4
+        Me.btnNum4.Text = "4"
+        Me.btnNum4.UseVisualStyleBackColor = True
+        '
+        'btnNum5
+        '
+        Me.btnNum5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum5.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum5.Location = New System.Drawing.Point(82, 82)
+        Me.btnNum5.Name = "btnNum5"
+        Me.btnNum5.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum5.TabIndex = 5
+        Me.btnNum5.Text = "5"
+        Me.btnNum5.UseVisualStyleBackColor = True
+        '
+        'btnNum6
+        '
+        Me.btnNum6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum6.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum6.Location = New System.Drawing.Point(161, 82)
+        Me.btnNum6.Name = "btnNum6"
+        Me.btnNum6.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum6.TabIndex = 6
+        Me.btnNum6.Text = "6"
+        Me.btnNum6.UseVisualStyleBackColor = True
+        '
+        'btnMult
+        '
+        Me.btnMult.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnMult.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMult.Location = New System.Drawing.Point(240, 82)
+        Me.btnMult.Name = "btnMult"
+        Me.btnMult.Size = New System.Drawing.Size(73, 73)
+        Me.btnMult.TabIndex = 7
+        Me.btnMult.Text = "×"
+        Me.btnMult.UseVisualStyleBackColor = True
+        '
+        'btnNum1
+        '
+        Me.btnNum1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum1.Location = New System.Drawing.Point(3, 161)
+        Me.btnNum1.Name = "btnNum1"
+        Me.btnNum1.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum1.TabIndex = 8
+        Me.btnNum1.Text = "1"
+        Me.btnNum1.UseVisualStyleBackColor = True
+        '
+        'btnNum2
+        '
+        Me.btnNum2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum2.Location = New System.Drawing.Point(82, 161)
+        Me.btnNum2.Name = "btnNum2"
+        Me.btnNum2.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum2.TabIndex = 9
+        Me.btnNum2.Text = "2"
+        Me.btnNum2.UseVisualStyleBackColor = True
+        '
+        'btnNum3
+        '
+        Me.btnNum3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnNum3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNum3.Location = New System.Drawing.Point(161, 161)
+        Me.btnNum3.Name = "btnNum3"
+        Me.btnNum3.Size = New System.Drawing.Size(73, 73)
+        Me.btnNum3.TabIndex = 10
+        Me.btnNum3.Text = "3"
+        Me.btnNum3.UseVisualStyleBackColor = True
+        '
+        'btnRest
+        '
+        Me.btnRest.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnRest.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRest.Location = New System.Drawing.Point(240, 161)
+        Me.btnRest.Name = "btnRest"
+        Me.btnRest.Size = New System.Drawing.Size(73, 73)
+        Me.btnRest.TabIndex = 11
+        Me.btnRest.Text = "-"
+        Me.btnRest.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnBorrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBorrar.Location = New System.Drawing.Point(319, 3)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(75, 73)
+        Me.btnBorrar.TabIndex = 14
+        Me.btnBorrar.Text = "<="
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnMas
+        '
+        Me.btnMas.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnMas.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMas.Location = New System.Drawing.Point(240, 240)
+        Me.btnMas.Name = "btnMas"
+        Me.btnMas.Size = New System.Drawing.Size(73, 75)
+        Me.btnMas.TabIndex = 15
+        Me.btnMas.Text = "+"
+        Me.btnMas.UseVisualStyleBackColor = True
+        '
+        'btnCE
+        '
+        Me.btnCE.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnCE.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCE.Location = New System.Drawing.Point(319, 161)
+        Me.btnCE.Name = "btnCE"
+        Me.btnCE.Size = New System.Drawing.Size(75, 73)
+        Me.btnCE.TabIndex = 16
+        Me.btnCE.Text = "CE"
+        Me.btnCE.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 5
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.btnIgual, 4, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnC, 4, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnCE, 4, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnMas, 3, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnBorrar, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnRest, 3, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum3, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum2, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum1, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnMult, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum6, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum5, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum4, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnDiv, 3, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum9, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum8, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum7, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnNum0, 0, 3)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 63)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(397, 318)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnIgual.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIgual.Location = New System.Drawing.Point(319, 240)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(75, 75)
+        Me.btnIgual.TabIndex = 18
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnC.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnC.Location = New System.Drawing.Point(319, 82)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(75, 73)
+        Me.btnC.TabIndex = 17
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 381)
+        Me.Controls.Add(Me.txtBoxResultado)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Calculadora by JotaMarti"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtBoxResultado As TextBox
+    Friend WithEvents btnNum0 As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnCE As Button
+    Friend WithEvents btnMas As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnRest As Button
+    Friend WithEvents btnNum3 As Button
+    Friend WithEvents btnNum2 As Button
+    Friend WithEvents btnNum1 As Button
+    Friend WithEvents btnMult As Button
+    Friend WithEvents btnNum6 As Button
+    Friend WithEvents btnNum5 As Button
+    Friend WithEvents btnNum4 As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btnNum9 As Button
+    Friend WithEvents btnNum8 As Button
+    Friend WithEvents btnNum7 As Button
+End Class
---
+++ CalculadoraJMMC/Form1.vb
@@ -0,0 +1,183 @@
+﻿Public Class Form1
+
+    Dim operador1? As Decimal
+    Dim operador2? As Decimal
+    Dim btnClicked As Button
+    Dim previousOperation As Button
+    Dim currentOperation As Button
+    Dim limpiaPantalla As Boolean = True
+    Dim numberEntered As Boolean = False
+    Dim maxLenght As Integer = 16
+
+
+    Private Sub btnNum_Click(sender As Object, e As EventArgs) Handles btnNum0.Click, btnNum1.Click, btnNum9.Click, btnNum8.Click, btnNum7.Click, btnNum6.Click, btnNum5.Click, btnNum4.Click, btnNum3.Click, btnNum2.Click
+
+        If limpiaPantalla = True Then
+            cleanTxtBox()
+        End If
+
+        btnClicked = TryCast(sender, Button)
+
+        ' Con esto no añado más numero si ha superado el largo máximo, en este caso he puesto 16
+        ' También realizo varias comprobaciones para no poner ceros a la izquierda
+        If txtBoxResultado.Text.Length < maxLenght Then
+            If btnClicked.Text = btnNum0.Text Then
+                If txtBoxResultado.Text = "0" Or
+                    txtBoxResultado.Text = "" Then
+                    txtBoxResultado.Text = "0"
+                Else
+                    txtBoxResultado.Text = txtBoxResultado.Text & btnClicked.Text
+                End If
+            Else
+                If txtBoxResultado.Text = "0" Then
+                    txtBoxResultado.Text = ""
+                End If
+                txtBoxResultado.Text = txtBoxResultado.Text & btnClicked.Text
+                numberEntered = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnOperation_Click(sender As Object, e As EventArgs) Handles btnMas.Click, btnMult.Click, btnRest.Click, btnDiv.Click
+
+        ' Si la operacion actual no es nula me guardo la operacion anterior
+        If currentOperation IsNot Nothing Then
+            previousOperation = currentOperation
+        End If
+
+        ' Me guardo la operacion actual
+        currentOperation = TryCast(sender, Button)
+
+        ' Esto es un check para cuando se concatenan operaciones, si damos varias veces al boton más por ejemplo no haga nada
+        If numberEntered = False Then
+            Exit Sub
+        End If
+
+        ' Si el operador1 esta vacio y hemos escrito algo me guardo el valor, si no se ha escrito nada vuelvo a pintar el 0
+        ' Y si todo es correcto llamo al sub del boton igual para realizar la operacion con el valor de la operacion anterior.
+        If operador1 Is Nothing AndAlso
+            txtBoxResultado.Text <> "0" Then
+            operador1 = Decimal.Parse(txtBoxResultado.Text)
+            limpiaPantalla = True
+        ElseIf operador1 Is Nothing AndAlso
+            txtBoxResultado.Text = "0" Then
+            txtBoxResultado.Text = "0"
+        Else
+            btnIgual_Click(previousOperation, e)
+        End If
+
+
+        btnClicked = TryCast(sender, Button)
+
+
+
+    End Sub
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+
+        Dim buttonOrigin As Button
+
+        buttonOrigin = TryCast(sender, Button)
+
+        Dim resultado As Decimal
+
+        ' Si tenemos operador1 guardamos operador2 y depende de si le hemos pulsado en igual o en los operador realizo dos cosas distintas
+        If operador1 IsNot Nothing Then
+
+            operador2 = Decimal.Parse(txtBoxResultado.Text)
+
+            If buttonOrigin.Text = "=" Then
+                resultado = makeOperation(operador1, operador2, currentOperation.Text)
+                txtBoxResultado.Text = resultado
+                limpiaPantalla = True
+                operador1 = Nothing
+                operador2 = Nothing
+            Else
+                resultado = makeOperation(operador1, operador2, previousOperation.Text)
+                txtBoxResultado.Text = resultado
+                limpiaPantalla = True
+                operador1 = resultado
+                numberEntered = False
+            End If
+        End If
+
+        btnClicked = TryCast(sender, Button)
+
+    End Sub
+
+    Private Function makeOperation(op1 As Decimal, op2 As Decimal, sender As String)
+
+        Dim Resultado As Decimal
+
+        If sender = "+" Then
+            Resultado = op1 + op2
+        ElseIf sender = "-" Then
+            Resultado = op1 - op2
+        ElseIf sender = "×" Then
+            Resultado = op1 * op2
+        ElseIf sender = "/" Then
+            Resultado = op1 / op2
+        End If
+
+        Return Resultado
+
+    End Function
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+
+        ' En el primer if realizo una comprobación, por que cuando el numero tenia coma se quedaba por ej 3, y generaba problemas,
+        ' lo que hago es comprobar si lo siguiente a borrar es un coma y en ese caso borro el numero y la coma
+        If txtBoxResultado.Text.Length > 1 Then
+            Dim esComa As Char
+
+            esComa = txtBoxResultado.Text.Chars(txtBoxResultado.Text.Length - 2)
+
+            If esComa = "," Then
+                txtBoxResultado.Text = txtBoxResultado.Text.Substring(0, txtBoxResultado.Text.Length - 2)
+            Else
+                txtBoxResultado.Text = txtBoxResultado.Text.Substring(0, txtBoxResultado.Text.Length - 1)
+            End If
+
+        ElseIf txtBoxResultado.Text.Length = 0 Then
+            txtBoxResultado.Text = "0"
+            limpiaPantalla = True
+        ElseIf txtBoxResultado.Text.Length = 1 Then
+            txtBoxResultado.Text = "0"
+            limpiaPantalla = True
+        End If
+
+        btnClicked = TryCast(sender, Button)
+
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+
+        txtBoxResultado.Text = "0"
+        limpiaPantalla = True
+
+        operador1 = Nothing
+        operador2 = Nothing
+
+    End Sub
+
+    Private Sub btnCE_Click(sender As Object, e As EventArgs) Handles btnCE.Click
+
+        txtBoxResultado.Text = "0"
+        limpiaPantalla = True
+
+        btnClicked = TryCast(sender, Button)
+
+
+    End Sub
+
+    Private Sub cleanTxtBox()
+
+        If txtBoxResultado.Text <> "0" Then
+            operador1 = Decimal.Parse(txtBoxResultado.Text)
+        End If
+        txtBoxResultado.Text = ""
+        limpiaPantalla = False
+
+    End Sub
+End Class
